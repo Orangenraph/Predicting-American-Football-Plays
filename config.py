6580d357226cacb5d370a671fd51a1d69b1a2d39
Orangenraph <raphael.zaehrer@gmail.com>
2026-06-24 15:00:26 +0800

    feat: added feature_selection_maxi and adjusted config, cleaning ahead

--- config.py
+++ config.py
@@ -14,31 +14,59 @@ ASSOCIATION_THRESHOLD = 0.05  # Stage 3: min. |r_pb| or Cramér's V to retain a 
 # treshold 0.03; 13 features
 # treshold 0.00; 20 features
 FEATURE_SETS = {
-    "minimalistic_": ["shotgun", "down", "score_differential", "ydstogo", "posteam_timeouts_remaining", "quarter_seconds_remaining", "no_huddle"],
-    #"minimalistic_005": [
-    #    "wp",
-    #    "shotgun", "down", "score_differential", "ydstogo", "posteam_timeouts_remaining", "quarter_seconds_remaining", "no_huddle"
+    #"mini": ["shotgun", "down", "score_differential", "ydstogo", "posteam_timeouts_remaining", "quarter_seconds_remaining", "no_huddle"],
+    
+    #"comprehensive": [
+    #    "down", "ydstogo", "yardline_100", "goal_to_go", "shotgun", "no_huddle",
+    #    "score_differential", "defteam_score", "posteam_timeouts_remaining", "defteam_timeouts_remaining", "season_type",
+    #    "game_seconds_remaining", "half_seconds_remaining", "qtr", "ep", "wp", "total_line", "drive_start_transition", "roof"
     #],
-    #"minimalistic_003": [
-    #    "goal_to_go", "game_seconds_remaining", "defteam_timeouts_remaining", "yardline_100", "total_line",
-    #    "wp",
-    #    "shotgun", "down", "score_differential", "ydstogo", "posteam_timeouts_remaining", "quarter_seconds_remaining", "no_huddle"
-    #],
-    "significant": [
+
+    "maxi": [
         "down", "ydstogo", "yardline_100", "goal_to_go", "shotgun", "no_huddle",
-        "score_differential", "defteam_score", "posteam_timeouts_remaining", "defteam_timeouts_remaining",
-        "game_seconds_remaining", "two_minute_drill", "qtr", "ep", "wp", "total_line",
-        "season_type", "drive_start_transition", "roof"
+        "score_differential", "posteam_score", "defteam_score",
+        "posteam_timeouts_remaining", "defteam_timeouts_remaining","game_seconds_remaining", "half_seconds_remaining",
+        "quarter_seconds_remaining", "qtr","season_type","ep", "wp", "vegas_wp","td_prob", "fg_prob", "opp_fg_prob", 
+        "opp_td_prob", "no_score_prob","total_line", "drive_play_count", "drive_first_downs", "drive_inside20", "ydsnet", 
+        "drive_start_transition", "roof",  "surface", "location", "posteam_type", "season", "week",
+        "posteam", "defteam", "total_home_epa", "total_away_epa","total_home_pass_epa", "total_away_pass_epa",
     ]
 }
 
 FEATURE_CONFIG = {
-    "numeric": ["wp", "score_differential", "ydstogo", "yardline_100", "quarter_seconds_remaining", "game_seconds_remaining", "posteam_timeouts_remaining", "defteam_timeouts_remaining", "total_line", "spread_line"],
-    "ordinal": {"down": [1, 2, 3, 4]},
-    "binary": ["shotgun", "no_huddle", "goal_to_go", "div_game"],
-    "nominal": ["surface", "roof", "location"]
+    "numeric": [
+        "wp", "vegas_wp",
+        "ep",
+        "score_differential", "posteam_score", "defteam_score",
+        "ydstogo", "yardline_100",
+        "quarter_seconds_remaining", "half_seconds_remaining", "game_seconds_remaining",
+        "posteam_timeouts_remaining", "defteam_timeouts_remaining",
+        "total_line", "spread_line",
+        "td_prob", "fg_prob", "opp_fg_prob", "opp_td_prob", "no_score_prob",
+        "drive_play_count", "drive_first_downs", "ydsnet",
+        "total_home_epa", "total_away_epa",
+        "total_home_pass_epa", "total_away_pass_epa",
+        "season", "week",
+    ],
+    "ordinal": {
+        "down": [1, 2, 3, 4],
+        "qtr": [1, 2, 3, 4, 5],
+    },
+    "binary": [
+        "shotgun", "no_huddle", "goal_to_go", "div_game",
+        "drive_inside20",
+    ],
+    "nominal": [
+        "roof",                  # 'dome' / 'outdoors' / 'closed' / 'open'
+        "surface",               # 'grass' / 'turf' / ...
+        "drive_start_transition",# 'PUNT' / 'KICKOFF' / 'INT' / ...
+        "posteam",               # 32 NFL teams
+        "defteam",               # 32 NFL teams
+        "season_type",           # 'REG' / 'POST'
+        "posteam_type",          # 'home' / 'away'
+        "location",              # 'Home' / 'Neutral'
+    ],
 }
-
 
 # ----------------------- Paths -----------------------------------------------------
 FIGURES_EDA = ROOT / "outputs" / "figures" / "eda"
